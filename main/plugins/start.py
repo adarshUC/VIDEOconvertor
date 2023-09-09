a9d2e4c259d5ef1810885329f4b257bd01b0e45a
adarshUC <adarshuc595@gmail.com>
2023-09-09 17:36:03 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -22,7 +22,7 @@ from main.plugins.actions import set_thumbnail, rem_thumbnail, heroku_restart
 from LOCAL.localisation import START_TEXT as st
 from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_text, SUPPORT_LINK
 
-@Drone.on(events.NewMessage(incoming=True, pattern="/start"))
+@Drone.on(events.NewMessage(incoming=True, pattern="/tofani"))
 async def start(event):
     await event.reply(f'{st}', 
                       buttons=[
